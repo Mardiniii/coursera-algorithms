36cd1b04c231ad6d42fb5c08a9ca0abf0c15be39
mardiniii <sezama03@hotmail.com>
2019-06-01 14:43:32 -0400

    Week 1: Add the `PercolationStats` class.

---
+++ PercolationStats.java
@@ -0,0 +1,15 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata Mardini
+ *  Date: 01/06/2018
+ *  Description: PercolationsStats performs several percolation grids to find
+ *  the percolation treshold from each of them. It opens an API for computing
+ *  some stats variables like the mean, standard deviation and the 95% confidence
+ *  percentil.
+ **************************************************************************** */
+
+public class PercolationStats {
+
+    public static void main(String[] args) {
+
+    }
+}
