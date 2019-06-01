1482350cf3b252d22e87dc1b61281639c0135dae
mardiniii <sezama03@hotmail.com>
2019-06-01 14:53:24 -0400

    Week 1: Define `PercolationStats` constructor for computing `trials` experiments and collecting percolation tresholds.

--- PercolationStats.java
+++ PercolationStats.java
@@ -7,10 +7,36 @@
  *  percentil.
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Arrays;
+
 public class PercolationStats {
     private int N;                   // Grid size
     private int trials;              // Number of experiments
     private int[] experimentResults; // Number of open sites when grid percolated
+
+    public PercolationStats(int n, int numberOfTrials) {
+        validInput(n, numberOfTrials);
+
+        N = n;
+        trials = numberOfTrials;
+        experimentResults = new int[trials];
+
+        for(int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(n);
+
+            while (!perc.percolates()) {
+                int row = randomNumber();
+                int col = randomNumber();
+
+                if (!perc.isOpen(row, col)) {
+                    perc.open(row, col);
+                    if (perc.percolates()) experimentResults[i] = perc.numberOfOpenSites();
+                }
+            }
+        }
+    }
 
     private void validInput(int n, int numberOfTrials) {
         if (n < 1 && numberOfTrials < 1) {
@@ -23,6 +49,8 @@ public class PercolationStats {
     }
 
     public static void main(String[] args) {
+        PercolationStats stats = new PercolationStats(10, 1000);
 
+        System.out.println(Arrays.toString(stats.experimentResults));
     }
 }
