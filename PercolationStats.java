e7f8acb0971dd21f5d9f34e7f7462205b65a2664
mardiniii <sezama03@hotmail.com>
2019-06-01 17:12:50 -0400

    Week 1: Memoize the `mean` value in the `PercolationStats` class. (Try to accomplish 100% grade - 99% currently).

--- PercolationStats.java
+++ PercolationStats.java
@@ -11,11 +11,12 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    static final double CONFIDENCE_95 = 1.96;
+    private static final double CONFIDENCE_95 = 1.96;
 
     private final int gridSize;                   // Grid size
     private final int trials;              // Number of experiments
     private double sttdev;
+    private double mean;
     private final double[] experimentResults; // Number of open sites when grid percolated
 
     public PercolationStats(int n, int numberOfTrials) {
@@ -45,7 +46,9 @@ public class PercolationStats {
     }
 
     public double mean() {
-        return StdStats.mean(experimentResults);
+        if (mean == 0.0) mean = StdStats.mean(experimentResults);
+
+        return mean;
     }
 
     public double stddev() {
