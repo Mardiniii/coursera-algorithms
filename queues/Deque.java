ec676ea03aa56a6d8f180a421556912766ad4a06
mardiniii <sezama03@hotmail.com>
2019-06-04 08:06:37 -0400

    Week 2: Create queues directory and define `Deque` class.

---
+++ queues/Deque.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata Mardini
+ *  Date: 06/04/2019
+ *  Description: Deque is a generalization of a stack and a queue that supports
+ *  adding and removing items from either the front or the back of the data
+ *  structure.
+ **************************************************************************** */
+
+public class Deque<Item> implements Iterable<Item> {
+
+    public static void main(String[] args) {
+
+    }
+}
