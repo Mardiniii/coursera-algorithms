7b65b023d781d12b93752cd9c51848000cc56d78
mardiniii <sezama03@hotmail.com>
2019-06-06 07:19:11 -0400

    Week 2: Define `RandomizedQueue` class.

---
+++ queues/RandomizedQueue.java
@@ -0,0 +1,16 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata
+ *  Date: 06/07/2019
+ *  Description: A randomized queue is similar to a stack or queue, except that
+ *  the item removed is chosen uniformly at random from items in the data
+ *  structure.
+ **************************************************************************** */
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] queue;
+    private int n = 0;
+
+    public static void main(String[] args) {
+
+    }
+}
