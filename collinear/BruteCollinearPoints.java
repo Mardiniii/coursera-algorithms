0b912066f2bc40e713e6165f2081ff4ec597426d
mardiniii <sezama03@hotmail.com>
2019-06-12 08:27:46 -0400

    Week 3: Define `BruteCollinearPoints` class.

---
+++ collinear/BruteCollinearPoints.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata Mardini
+ *  Date: 06/12/2019
+ *  Description: Examines 4 points at a time and checks whether they all lie on
+ *  the same line segment, returning all such line segments.
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+    public static void main(String[] args) {
+
+    }
+}
