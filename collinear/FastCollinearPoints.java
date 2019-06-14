e78ed5e53e8dac4fe4bfe4718419aebb383509c8
mardiniii <sezama03@hotmail.com>
2019-06-14 06:26:23 -0400

    Week 3: Define `FastCollinearPoints` class.

---
+++ collinear/FastCollinearPoints.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata Mardini
+ *  Date: 06/14/2019
+ *  Description: Examines 4 points at a time and checks whether they all lie on
+ *  the same line segment, returning all such line segments.
+ **************************************************************************** */
+
+public class FastCollinearPoints {
+
+
+    public static void main(String[] args) {
+
+    }
+}
