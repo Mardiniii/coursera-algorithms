ad7abe361f37d2c6a4f1cd342afe389789879f6e
mardiniii <sezama03@hotmail.com>
2019-06-14 07:39:06 -0400

    Week 3: Add the `FastCollinearPoints#validatePoints` method.

--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -8,6 +8,33 @@
 public class FastCollinearPoints {
 
 
+    public FastCollinearPoints(Point[] pointsArray) {
+        validatePoints(pointsArray);
+    }
+
+    private void validatePoints(Point[] pointsArray) {
+        // Check if the `pointsArray` is a null argument
+        if (pointsArray == null) {
+            throw new IllegalArgumentException("The points argument is invalid, null value passed!");
+        }
+
+        // Check for any null elements in the `pointsArray`
+        for (int i = 0; i < pointsArray.length; i++) {
+            if (pointsArray[i] == null) {
+                throw new IllegalArgumentException("Invalid point element, it contains null values!");
+            }
+        }
+
+        // Check if the `pointsArray` contains duplicated elements.
+        for (int i = 0; i < pointsArray.length; i++) {
+            for (int j = i + 1; j < pointsArray.length; j++) {
+                if (pointsArray[i] == pointsArray[j]) {
+                    throw new IllegalArgumentException("Duplicated point elements!");
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
     }
