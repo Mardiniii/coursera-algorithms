e74a8c0fe005cc7ffa4664d862123fd99479633e
mardiniii <sezama03@hotmail.com>
2019-06-18 07:37:47 -0400

    Week 4: Include the `Board` class for solving the Puzzle assignment.

---
+++ puzzle/Board.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata Mardini
+ *  Date: 06/18/2019
+ *  Description: Board is an abstraction for representing a n-by-n array of blocks.
+ *  This abstraction is used to solve the 8-Puzzle problem by using MinPQ and MaxPQ.
+ **************************************************************************** */
+
+public class Board {
+    public static void main(String[] args) {
+
+    }
+}
