1a44c49ccdf600cef0289f4ca5a87119f1b7a497
mardiniii <sezama03@hotmail.com>
2019-06-18 08:01:38 -0400

    Week 4: Define `Board` constructor.

--- puzzle/Board.java
+++ puzzle/Board.java
@@ -6,6 +6,21 @@
  **************************************************************************** */
 
 public class Board {
+    int n; // Board dimension
+    int[][] grid;
+
+    // Construct a board from an n-by-n array of blocks
+    public Board(int[][] blocks) {
+        this.n = blocks.length;
+        this.grid = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                grid[i][j] = blocks[i][j];
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
     }
