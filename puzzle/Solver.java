3be6f52cc84a17579b705405b1fd8e29842a1f6a
mardiniii <sezama03@hotmail.com>
2019-06-24 08:03:44 -0400

    Week 4: Add the `Solver` class.

---
+++ puzzle/Solver.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata Mardini
+ *  Date: 06/24/2019
+ *  Description: Solver is an abstraction to solve the 8-Puzzle problem by using
+ *  the A* algorithm.
+ **************************************************************************** */
+
+public class Solver {
+    public static void main(String[] args) {
+
+    }
+}
