2b3a46a9ce5d9e7ea58ddb1d451d97e89187070d
mardiniii <sezama03@hotmail.com>
2019-07-02 07:25:12 -0400

    Week 5: Add `PointSET` constructor method.

--- kd-tree/PointSET.java
+++ kd-tree/PointSET.java
@@ -5,7 +5,16 @@
  *  in the unit square by using a red-black BST.
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.SET;
+
 public class PointSET {
+    private SET<Point2D> tree;
+
+    public PointSET() {
+        this.tree = new SET<Point2D>();
+    }
+
     public static void main(String[] args) {
 
     }
