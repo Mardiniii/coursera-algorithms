c92d3b3a973fc2c02cf9a01e259107fc6ca0dd16
mardiniii <sezama03@hotmail.com>
2019-07-02 07:35:20 -0400

    Week 5: Implement the `PointSET#isEmpty` method.

--- kd-tree/PointSET.java
+++ kd-tree/PointSET.java
@@ -11,8 +11,14 @@ import edu.princeton.cs.algs4.SET;
 public class PointSET {
     private SET<Point2D> tree;
 
+    // Initialize and empty PointSET data structure.
     public PointSET() {
         this.tree = new SET<Point2D>();
+    }
+
+    // Returns `true` or `false` if the PointSET is empty or not.
+    public boolean isEmpty() {
+        return tree.isEmpty();
     }
 
     public static void main(String[] args) {
