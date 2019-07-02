2df62f5427f268ce706f812a1a9e68e1631d1ee3
mardiniii <sezama03@hotmail.com>
2019-07-02 07:17:11 -0400

    Week 5: Include the `PointSET` class for solving the Kd-Tree assignment (Brute force approach).

---
+++ kd-tree/PointSET.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata Mardini
+ *  Date: 07/02/2019
+ *  Description: `PointSET` is a mutable datatyp that represents a set of points
+ *  in the unit square by using a red-black BST.
+ **************************************************************************** */
+
+public class PointSET {
+    public static void main(String[] args) {
+
+    }
+}
