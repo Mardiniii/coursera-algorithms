52214ffa54cac2640b52f7014c6959655398c520
mardiniii <sezama03@hotmail.com>
2019-07-04 11:48:41 -0400

    Week 5: Include the `KdTree` class for solving the Kd-Tree assignment (2d-Tree implementation approach).

---
+++ kd-tree/KdTree.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name: Sebastian Zapata Mardini
+ *  Date: 07/04/2019
+ *  Description: `KdTree` is a mutable datatype that represents a set of points
+ *  in the unit square by using a 2d-Tree.
+ **************************************************************************** */
+
+public class KdTree {
+    public static void main(String[] args) {
+
+    }
+}
--- kd-tree/PointSET.java
+++ kd-tree/PointSET.java
@@ -1,7 +1,7 @@
 /* *****************************************************************************
  *  Name: Sebastian Zapata Mardini
  *  Date: 07/02/2019
- *  Description: `PointSET` is a mutable datatyp that represents a set of points
+ *  Description: `PointSET` is a mutable datatype that represents a set of points
  *  in the unit square by using a red-black BST.
  **************************************************************************** */
 
